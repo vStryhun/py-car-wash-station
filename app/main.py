13265f0741604baf42831ec0728bedfd28edd9d5
Vita <st.vitalina666@gmail.com>
2024-07-24 09:25:21 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,63 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str
+    ) -> None:
+        if not (1 <= comfort_class <= 7):
+            print("Error")
+        if not (1 <= clean_mark <= 10):
+            print("Error")
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+        self,
+        distance_from_city_center: float,
+        clean_power: bool,
+        average_rating: float,
+        count_of_ratings: int,
+    ) -> None:
+        if not (1.0 <= distance_from_city_center <= 10.0):
+            print("Error")
+        if not (1 <= clean_power <= 10):
+            print("Error")
+        if not (1.0 <= average_rating <= 5.0):
+            print("Error")
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> float:
+        income = 0.0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+        return round(income, 1)
+
+    def calculate_washing_price(self, car: Car) -> float:
+        cost = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
+        )
+        return round(cost, 1)
+
+    def wash_single_car(self, car: Car) -> None:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, new_rating: float) -> None:
+        if not (1.0 <= new_rating <= 5.0):
+            print("Error")
+        total_rating = self.average_rating * self.count_of_ratings
+        total_rating += new_rating
+        self.count_of_ratings += 1
+        self.average_rating = round(total_rating / self.count_of_ratings, 1)
